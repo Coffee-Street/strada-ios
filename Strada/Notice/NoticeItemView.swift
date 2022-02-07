5c6634434267f6c1be82c03ad3cd50d0c2b93777
whdqhdqkr <whdqhd.qkr@gmail.com>
2022-02-07 22:43:32 +0900

    notice view
    - notice view 화면 개발
    - home에서 notice 화면으로 넘어갈 수 있게 처리
    - category에 따라 적절한 값이 나오게 처리

---
+++ Strada/Notice/NoticeItemView.swift
@@ -0,0 +1,61 @@
+//
+//  NoticeItemView.swift
+//  Strada
+//
+//  Created by 박종봉 on 2022/02/07.
+//
+
+import SwiftUI
+
+struct NoticeItemView : View {
+    
+    let notice: Notice
+    
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text(notice.title)
+                        .foregroundColor(getBlackFontColor())
+                        .fontWeight(.medium)
+                        .overlay {
+                            ZStack {
+                                CircleBadgeView()
+                                    .opacity(notice.detect ? 0.0 : 1.0)
+                            }
+                            .alignmentGuide(.top) { $0.height / 2 - 3 }
+                            .alignmentGuide(.trailing) { $0.width / 2 - 5 }
+                            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
+                        }
+                    Text(notice.excerpt)
+                        .foregroundColor(getBlackFontColor())
+                        .fontWeight(.regular)
+                        .padding(.bottom, 2)
+                    Text(notice.date)
+                        .foregroundColor(getGrayFontColor())
+                        .fontWeight(.medium)
+                }
+                
+                Spacer()
+                
+                Image("next.active")
+            } // HStack
+        } // VStack
+    }
+    
+    func getBlackFontColor() -> Color {
+        return notice.read ? .appBrownGray : .appBlack
+    }
+    
+    func getGrayFontColor() -> Color {
+        return notice.read ? .appLightGray : .appBrownGray
+    }
+}
+
+struct NoticeItemView_Previews : PreviewProvider {
+    static let notice = Notice(type: NoticeType.ORDER, title: "주문이 성공적으로 처리되었습니다", excerpt: "아메리카노 외 3개 메뉴", date: "2020.10.17", read: false)
+    
+    static var previews: some View {
+        NoticeItemView(notice: notice)
+    }
+}
