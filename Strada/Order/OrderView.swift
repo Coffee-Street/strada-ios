855791db34ca6c78d35ca08cf566b5907c84e424
whdqhdqkr <whdqhd.qkr@gmail.com>
2022-02-13 23:45:12 +0900

    장바구니 버튼 살리기
    - NavigationView를 사용해야 하는데 OrderView에 정의되어 있고 HomeView에 있지 않아서 이동

--- Strada/Order/OrderView.swift
+++ Strada/Order/OrderView.swift
@@ -15,36 +15,33 @@ struct OrderView : View {
     @State private var viewModel = OrderViewModel()
     
     var body: some View {
-        NavigationView {
-            ZStack(alignment: .top) {
-                ScrollView {
-                    ZStack(alignment: .top) {
-                        VStack(alignment: .leading, spacing: 0) {
-                            MenuView(controller: controller)
-                        } // VStack
-                        .padding(.horizontal)
-                        .frame(minWidth: 0, maxWidth: .infinity, minHeight:0, alignment: Alignment.topLeading)
-                        
-                        VStack(alignment: .leading, spacing: 0) {
-                            HStack {
-                                Spacer()
-                                Button(action: {
-                                    isOpened = false
-                                }) {
-                                    Image(systemName: "multiply")
-                                        .foregroundColor(.appBlue)
-                                        .font(.system(size: 30))
-                                }
-                            }
-                            .padding(.trailing)
+        ZStack(alignment: .top) {
+            ScrollView {
+                ZStack(alignment: .top) {
+                    VStack(alignment: .leading, spacing: 0) {
+                        MenuView(controller: controller)
+                    } // VStack
+                    .padding(.horizontal)
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight:0, alignment: Alignment.topLeading)
+                    
+                    VStack(alignment: .leading, spacing: 0) {
+                        HStack {
                             Spacer()
-                        } // VStack
-                    } // ZStack
-                    .background(.white)
-                } // ScrollView
-            }
-        } // NavigationView
-        
+                            Button(action: {
+                                isOpened = false
+                            }) {
+                                Image(systemName: "multiply")
+                                    .foregroundColor(.appBlue)
+                                    .font(.system(size: 30))
+                            }
+                        }
+                        .padding(.trailing)
+                        Spacer()
+                    } // VStack
+                } // ZStack
+                .background(.white)
+            } // ScrollView
+        } // ZStack    
     }
 }
 
