a9fa101047da8646a4e3f8a8a95d1c8d10b313d3
whdqhdqkr <whdqhd.qkr@gmail.com>
2022-03-05 23:10:02 +0900

    kakaopay test api
    - 기본 구조 생성

---
+++ Strada/Payment/KakaoPayment.swift
@@ -0,0 +1,14 @@
+//
+//  KakaoPayment.swift
+//  Strada
+//
+//  Created by 박종봉 on 2022/03/13.
+//
+
+import Foundation
+
+struct KakaoPayment : Identifiable {
+    let id = UUID()
+    
+    let tid: String
+}
--- Strada/Payment/PaymentAPI.swift
+++ Strada/Payment/PaymentAPI.swift
@@ -15,6 +15,31 @@ struct PaymentResponse : Codable {
     
 }
 
+struct KakaoPaymentReadyRequest : Codable {
+    let cid: String
+    let partner_order_id: String
+    let partner_user_id: String
+    let item_name: String
+    let quantity: Int
+    let total_amount: Int
+    let vat_amount: Int
+    let tax_free_amount: Int
+    
+    let approval_url: String
+    let fail_url: String
+    let cancel_url: String
+}
+
+struct KakaoPaymentReadyResponse : Codable {
+    let tid: String
+    let next_redirect_app_url: String
+    let next_redirect_mobile_url: String
+    let next_redirect_pc_url: String
+    let android_app_scheme: String
+    let ios_app_scheme: String
+    let created_at: Date
+}
+
 struct PaymentAPI {
     let api = API()
     
@@ -51,5 +76,18 @@ struct PaymentAPI {
             completion(.success(Payment()))
         }
         .resume()
+    }
+    
+    func kakaoPayRequest() {
+        //TODO: 결제 준비 이후 대기화면으로 이동하고 기다리기
+        //      결제에 대한 3가지 상태에 따라 다른 곳으로 이동 가능함
+        //      성공 -> approval_url 로 이동
+        //      취소 -> cancel_url 로 이동
+        //      요청 15분 이상 경과 -> fail_url 로 이동
+        //      문제는 URL 방식이라서 App에서는 어떻게 돌려받을지 고민해봐야 할거 같음
+    }
+    
+    func kakaoPayApprove() {
+        //TODO: 결제 승인 후 pg_token을 받아오고 BE에 요청하기
     }
 }
