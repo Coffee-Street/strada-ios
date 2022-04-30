97ce93dcd7a05b761b5967639fcd4421cf13a44b
whdqhdqkr <whdqhd.qkr@gmail.com>
2022-04-30 22:56:00 +0900

    ProfileView 개선
    - 좌우 padding 수정
    - 로그아웃 버튼 추가

--- Strada/Profile/ProfileView.swift
+++ Strada/Profile/ProfileView.swift
@@ -11,60 +11,77 @@ struct ProfileView : View {
     
     @ObservedObject var controller: CurrentViewController
     
-    @Binding var isOpened: Bool
-    
     @State private var isOpenedReceipt: Bool = false
     
     @StateObject private var viewModel = ProfileViewModel()
     
+    @Environment(\.presentationMode) var presentationMode
+    
     var body: some View {
         ZStack(alignment: .leading) {
-            VStack(alignment: .leading) {
+            VStack(alignment: .leading, spacing: 0) {
                 Text("내 정보")
                     .foregroundColor(.appBlue)
                     .font(.system(size: 22, weight: .bold))
-                    .padding(.bottom)
+                    .padding(.bottom, 28)
                 
                 Text("\(self.viewModel.profile.point)P")
                     .font(.system(size: 35))
-                    .padding(.bottom, 5)
+                    .padding(.bottom, 8)
                 
                 Text("포인트 관련 공지를 작성합니다.\n포인트는 어떠어떠할 때 사용하실 수 없습니다.")
                     .foregroundColor(.appBrownGray)
                     .font(.system(size: 13))
                 
                 Divider()
-                    .background(Color.appBrownGray)
-                    .padding(.vertical)
+                    .frame(height: 1)
+                    .background(Color.appVeryLightGray)
+                    .padding(.vertical, 24)
                 
                 HistoryView(isOpenedReceipt: $isOpenedReceipt)
+                    .padding(.trailing, 24)
                 
                 Spacer()
             }
-            .padding()
+            .padding(.leading, 24)
+//            .padding(.leading, 24)
             
             VStack {
-                HStack {
-                    Spacer()
-                    Button(action: {
-                        isOpened = false
-                    }) {
-                        Image(systemName: "multiply")
-                            .foregroundColor(.appBlue)
-                            .font(.system(size: 30))
+                VStack(spacing: 0) {
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            presentationMode.wrappedValue.dismiss()
+                        }) {
+                            Image(systemName: "multiply")
+                                .foregroundColor(.appBlue)
+                                .font(.system(size: 30))
+                        }
                     }
-                }
-                .padding(.top)
-                
+                    .padding(.bottom, 24)
+                    
+                    HStack {
+                        Spacer()
+                        Button(action: {
+//                            presentationMode.wrappedValue.dismiss()
+                            controller.goSign()
+                        }) {
+                            Text("로그아웃")
+                                .foregroundColor(.appOrange)
+                                .font(.system(size: 15))
+                        }
+                    }
+                } // VStack
                 Spacer()
             } // VStack
-            .padding(.trailing)
+            .padding(.trailing, 24)
             
             if isOpenedReceipt {
                 ReceiptView(controller: controller, isOpened: $isOpenedReceipt)
             }
         } // ZStack
         .background(.white)
+        .edgesIgnoringSafeArea(.bottom)
         .onAppear {
             viewModel.getProfile()
         }
@@ -73,6 +90,6 @@ struct ProfileView : View {
 
 struct ProfileView_Previews : PreviewProvider {
     static var previews: some View {
-        ProfileView(controller: CurrentViewController("profile"), isOpened: .constant(false))
+        ProfileView(controller: CurrentViewController("profile"))
     }
 }
