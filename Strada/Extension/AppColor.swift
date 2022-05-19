823f6743dfcbf1df3de96ced4d8998f6ab45521a
whdqhdqkr <whdqhd.qkr@gmail.com>
2022-05-19 11:27:48 +0900

    Dark Mode 를 위한 기존 Color 제거
    - 기존에 extensions로만 관리하던 Color를 제거
    - 조금 더 의미있는 color 변수명을 사용하기 위해 색을 중복해서 관리하더라도 독립적으로 생성

--- Strada/Extension/AppColor.swift
+++ Strada/Extension/AppColor.swift
@@ -9,37 +9,16 @@ import SwiftUI
 
 extension Color {
     
-    static let appBlack = Color(hex: "#222222")
-  
-    static let appBlue = Color(hex: "#0047bb")
+    static let appTextPrimary = Color("AppPrimaryText")
+    static let appTextSecondary = Color("AppSecondaryText")
     
-    static let appLightBlue = Color(hex: "#3270d6")
-  
-    static let appOrange = Color(hex: "#ff6600")
+    static let appPrimary = Color("AppBlue")
+    static let appSecondary = Color("AppLightBlue")
+    static let appAlert = Color("AppOrange")
+    static let appDominant = Color("AppBeige")
+    static let appSemiHighlight = Color("AppBrownGray")
+    static let appHighlight = Color("AppVeryLightGray")
+    
+    static let appDisable = Color("AppLightGray")
 
-    static let appBeige = Color(hex: "#d6d2c4")
-    
-    static let appBrownGray = Color(hex: "#9f9f9f")
-    
-    static let appLightGray = Color(hex: "#cfcfcf")
-    
-    static let appVeryLightGray = Color(hex: "#eeeeee")
-}
-
-extension Color {
-
-    init(hex: String) {
-        let scanner = Scanner(string: hex)
-        _ = scanner.scanString("#")
-        
-        var rgb: UInt64 = 0
-        
-        scanner.scanHexInt64(&rgb)
-        
-        let r = Double((rgb >> 16) & 0xff) / 255.0
-        let g = Double((rgb >> 8) & 0xff) / 255.0
-        let b = Double((rgb >> 0) & 0xff) / 255.0
-        
-        self.init(red: r, green: g, blue: b)
-    }
 }
